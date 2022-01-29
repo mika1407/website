0d1ad18e510654dc529088691a6e339217c77ae7
mika1407 <koneautomaatio@hotmail.com>
2022-01-29 05:48:43 +0200

    footer tehty loppuun

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
 import React, {useState} from 'react';
+import Footer from "../components/Footer";
 import HeroSection from '../components/HeroSection';
 import InfoSection from '../components/InfoSection';
 import { homeObjOne, homeObjTwo, homeObjThree} from '../components/InfoSection/Data';
 import Navbar from '../components/Navbar';
 import Services from '../components/Services';
 import Sidebar from "../components/Sidebar";
-import Footer from "../components/Footer";
-
 
 const Home = () => { 
   const [isOpen, setIsOpen] = useState(false)
