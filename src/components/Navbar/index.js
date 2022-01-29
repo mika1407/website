346bd9f5c74ffbf723677ed633d3b06816842a12
mika tiihonen <koneautomaatio@hotmail.com>
2022-01-29 21:02:16 +0200

    Navbarin muutos scrolliin

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import {FaBars} from "react-icons/fa"
-import {Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink} from "./NavbarElements"
+import React, {useState, useEffect} from 'react';
+import {FaBars} from "react-icons/fa";
+import { IconContext } from 'react-icons/lib';
+import {Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink} from "./NavbarElements";
 
 const Navbar = ({toggle}) => {
+  const [scrollNav, setScrollNav] = useState(false)
+
+  const changeNav = () => {
+    if(window.scrollY >= 80) {
+      setScrollNav(true)
+    } else {
+      setScrollNav(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', changeNav)
+  }, [])
+
   return (
     <>
-      <Nav>
+    <IconContext.Provider value={{ color: '#fff'}}>
+      <Nav scrollNav={scrollNav}>
         <NavbarContainer>
           <NavLogo to="/">euros</NavLogo>
           <MobileIcon onClick={toggle}>
@@ -30,6 +46,7 @@ const Navbar = ({toggle}) => {
           </NavBtn>
         </NavbarContainer>
       </Nav>
+      </IconContext.Provider>
     </>
   )
 };
