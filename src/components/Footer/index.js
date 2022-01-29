ce479b4931d2778879bdae2c00b91dfc2eaa2991
mika tiihonen <koneautomaatio@hotmail.com>
2022-01-29 21:39:33 +0200

    Lisätty React Scroll

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { animateScroll as scroll } from 'react-scroll';
 import {FaFacebook, FaInstagram, FaYoutube, FaTwitter, FaLinkedin} from 'react-icons/fa';
 import { FooterContainer, FooterWrap, FooterLinksContainer, FooterLinksWrapper, FooterLinkItems, FooterLinkTitle, FooterLink,
 SocialMedia, SocialMediaWrap, SocialLogo, WebsiteRights, SocialIcons, SocialIconLink } from './FooterElements';
 
 const Footer = () => {
+
+    const toggleHome = () => {
+    scroll.scrollToTop();
+  }
+
   return (
     <FooterContainer>
       <FooterWrap>
@@ -44,7 +50,7 @@ const Footer = () => {
         </FooterLinksContainer>
         <SocialMedia>
           <SocialMediaWrap>
-            <SocialLogo to="/">
+            <SocialLogo to="/" onClick={toggleHome}>
               euros
             </SocialLogo>
             <WebsiteRights>euros &copy; {new Date().getFullYear()} All rights reserved.</WebsiteRights>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {FaBars} from "react-icons/fa";
 import { IconContext } from 'react-icons/lib';
+import { animateScroll as scroll } from 'react-scroll';
 import {Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink} from "./NavbarElements";
 
 const Navbar = ({toggle}) => {
@@ -16,29 +17,41 @@ const Navbar = ({toggle}) => {
 
   useEffect(() => {
     window.addEventListener('scroll', changeNav)
-  }, [])
+  }, []);
+
+  const toggleHome = () => {
+    scroll.scrollToTop();
+  }
 
   return (
     <>
     <IconContext.Provider value={{ color: '#fff'}}>
       <Nav scrollNav={scrollNav}>
         <NavbarContainer>
-          <NavLogo to="/">euros</NavLogo>
+          <NavLogo to="/" onClick={toggleHome}>euros</NavLogo>
           <MobileIcon onClick={toggle}>
             <FaBars />
           </MobileIcon>
           <NavMenu>
             <NavItem>
-              <NavLinks to="about">About</NavLinks>
+              <NavLinks to="about"
+              smooth={true} duration={500} spy={true} exact='true' offset={0}
+              >About</NavLinks>
             </NavItem>
             <NavItem>
-              <NavLinks to="discover">Discover</NavLinks>
+              <NavLinks to="discover" 
+              smooth={true} duration={500} spy={true} exact='true' offset={0}
+              >Discover</NavLinks>
             </NavItem>
             <NavItem>
-              <NavLinks to="services">Services</NavLinks>
+              <NavLinks to="services"
+              smooth={true} duration={500} spy={true} exact='true' offset={0}
+              >Services</NavLinks>
             </NavItem>
             <NavItem>
-              <NavLinks to="signup">Sign Up</NavLinks>
+              <NavLinks to="signup"
+              smooth={true} duration={500} spy={true} exact='true' offset={0}
+              >Sign Up</NavLinks>
             </NavItem>
           </NavMenu>
           <NavBtn>
